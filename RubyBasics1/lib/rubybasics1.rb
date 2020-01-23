5e6aa5e005b516b57e8580517e6cbcc37ba020fa
Visa Xiong <vxiong4@uncc.edu>
2020-01-23 15:57:27 +0000

    RubyBasics1-Part1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,19 @@
+# Lab 1
+# Part I
+def sum arr
+  num = 0
+  arr.each do |i|
+    num += i
+  end
+  return num
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
