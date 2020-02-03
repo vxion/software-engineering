616f206a9374e18b2156cf9b8c3e57517f1edaa2
Visa Xiong <vxiong4@uncc.edu>
2020-02-03 03:57:15 +0000

    Ruby Basics 2 - Part2

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -9,9 +9,32 @@ end
 # Part II
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if(s.to_s.empty?) 
+    return nil
+  else
+    char = s[0]
+    if (char.match(/[aAeEiIoOuU\W]/) == nil)
+      return true
+    else
+      return false
+    end
+end
+
 end
 
 # Part III
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+    if(s.eql?("0"))
+    return true
+  end
+  if(s.match(/[^0-1]/))
+    return false
+  else
+    if(s.match(/^[10]*00$/))
+      return true
+    else
+      return false
+    end
+  end
 end
