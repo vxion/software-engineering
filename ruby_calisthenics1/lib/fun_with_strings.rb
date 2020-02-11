2f1839365237497e2b0ff03706c91d55709e46a3
Visa Xiong <vxiong4@uncc.edu>
2020-02-11 01:18:48 +0000

    Lab 9: Fun with words - count words

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -2,13 +2,22 @@ module FunWithStrings
   def palindrome?
     # your code here
     # use downcase, gsub, and reverse for self.
-     self.downcase.gsub(/[^a-z]/, '') == self.downcase.gsub(/[^a-z]/, '').reverse
+    test = self.downcase.gsub(/[^a-z]/, '')
+     test == test.reverse
   end
   def count_words
     # your code here
+    test = Hash.new(0)
+    other = self.downcase.gsub(/[^a-z\s]/,'')
+    other.split.each do |x|
+      test[x] += 1
+      end
+      test
   end
   def anagram_groups
     # your code here
+      test = self.downcase.gsub(/[^a-z\s]/,'')
+      test.split.group_by{ |x| x.chars.sort }.values
   end
 end
 
