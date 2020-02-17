00449b21caae22171dfbc807daab63ade67be433
Visa Xiong <vxiong4@uncc.edu>
2020-02-17 18:48:44 +0000

    Ruby 00 Basics - Part 2

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -5,6 +5,8 @@ class BookInStock
 attr_accessor :isbn, :price
 
  def initialize isbn, price
+    
+    raise ArgumentError if isbn.empty? || price <= 0
     @isbn = isbn
     @price = price
     end
